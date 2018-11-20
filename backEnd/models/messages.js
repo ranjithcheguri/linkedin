f4552d5f5116b61906e50375034bf156595afe6f
Suhas Nayak <snsuhasnayak@gmail.com>
2018-11-20 15:06:03 -0800

    Updated messages schema

--- backEnd/models/messages.js
+++ backEnd/models/messages.js
@@ -1,13 +1,15 @@
-var mongoose = require('mongoose');
+var mongoose=require('mongoose');
+var Schema = mongoose.Schema;
 
-const messagesschema= new mongoose.Schema({
+//Booking schema
+var messgaesSchema = Schema({
     participants : [String],
     messages : [{
         from    : String,
         msg     : String,
         status  : String
-    }],
-})
-var messages = mongoose.model('messages',messagesschema);
+    }]
+});
 
-module.exports = {messages};
+
+module.exports= mongoose.model('Message', messgaesSchema,'Messages');
