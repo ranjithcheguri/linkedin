448166287768fb0d080eb075b195f8d7c69a0e10
Ranjith Cheguri <theeverindian@gmail.com>
2018-11-26 23:18:15 -0800

    profile page updated

--- frontEnd/src/reducers/index.js
+++ frontEnd/src/reducers/index.js
@@ -2,8 +2,6 @@
 import { combineReducers } from 'redux';
 import loginReducer from './loginReducer';
 
-
-
 //Reducer, just combines all the states.
 export default combineReducers({
     loginState: loginReducer,
