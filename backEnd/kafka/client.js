ed5fcaff87e02fec246db9ccc31e5054d2cd93d6
Vinay Kovuri <kovurivinay19@gmail.com>
2018-11-29 02:22:27 -0800

    Changes in Backend

--- backEnd/kafka/client.js
+++ backEnd/kafka/client.js
@@ -1,18 +1,18 @@
-var rpc = new (require('./kafkarpc'))();
+// var rpc = new (require('./kafkarpc'))();
 
-//make request to kafka
-function make_request(queue_name, msg_payload, callback){
-    console.log('Inside Client js Make request');
-    console.log(msg_payload);
-	rpc.makeRequest(queue_name, msg_payload, function(err, response){
+// //make request to kafka
+// function make_request(queue_name, msg_payload, callback){
+//     console.log('Inside Client js Make request');
+//     console.log(msg_payload);
+// 	rpc.makeRequest(queue_name, msg_payload, function(err, response){
 
-		if(err)
-			console.error(err);
-		else{
-			//console.log("Response in Client js: ", response);
-			callback(null, response);
-		}
-	});
-}
+// 		if(err)
+// 			console.error(err);
+// 		else{
+// 			//console.log("Response in Client js: ", response);
+// 			callback(null, response);
+// 		}
+// 	});
+// }
 
-exports.make_request = make_request;
+// exports.make_request = make_request;
